03a81a50481768b52ebeff148f3063de432a6309
LiamBooth <liam.booth@visualsoft.co.uk>
2020-02-24 14:20:48 +0000

    Update config.php

--- gscommerce_v3/frontend/config/config.php
+++ gscommerce_v3/frontend/config/config.php
@@ -1,10 +1,10 @@
 <?php
 
   // database parameters
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'fluidsit_liam');
-  define('DB_PASS', 'uh8eh8y4944y');
-  define('DB_NAME', 'fluidsit_app');
+  define('DB_HOST', '########');
+  define('DB_USER', '########');
+  define('DB_PASS', '########');
+  define('DB_NAME', '########');
 
 // app root
   define('APPROOT', dirname(dirname(__FILE__)));// using the dirname twice goes back up a folder
